35fc122250ca098bec94546522a20e2af8ab6d05
batoula1 <102437354+batoula1@users.noreply.github.com>
2024-07-20 23:45:12 -0400

    glab303.9.2

---
+++ GlabInheritance/src/Doctor.java
@@ -0,0 +1,8 @@
+public class Doctor {
+    String DoctorName;
+    String Department;
+
+    public void DoctorDetails(){
+        System.out.println("Doctor Details ...");
+    }
+}
---
+++ GlabInheritance/src/Hospital.java
@@ -0,0 +1,7 @@
+public class Hospital {
+    public static void main(String[] args) {
+        Surgeon s = new Surgeon();
+        s.DoctorDetails();
+        s.Surgeon_Details();
+    }
+}
